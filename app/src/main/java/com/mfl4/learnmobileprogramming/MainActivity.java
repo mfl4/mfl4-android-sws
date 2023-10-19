6801b9d74e0836a17655e23c9c62d5950f07aa0a
mfl4 <mhdfarhanl6267@gmail.com>
2023-10-20 04:22:04 +0700

    feat: #3 Download & Install Software (JDK, Android Studio, SDK, ADB, Emulator)

---
+++ app/src/main/java/com/mfl4/learnmobileprogramming/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mfl4.learnmobileprogramming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
