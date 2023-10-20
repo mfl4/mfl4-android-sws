ecac39081787e24074ea15b6794b3a99d852dbab
mfl4 <mhdfarhanl6267@gmail.com>
2023-10-20 12:50:36 +0700

    feat: #6 Activity dan Activity Life Cycle

--- app/src/main/java/com/mfl4/learnmobileprogramming/MainActivity.java
+++ app/src/main/java/com/mfl4/learnmobileprogramming/MainActivity.java
@@ -3,6 +3,8 @@ package com.mfl4.learnmobileprogramming;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Action", "onCreate");
+        Toast.makeText(this, "On Create", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "On Start", Toast.LENGTH_SHORT).show();
+        Log.i("Action", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show();
+        Log.i("Action", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show();
+        Log.i("Action", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "On Stop", Toast.LENGTH_SHORT).show();
+        Log.i("Action", "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "On Restart", Toast.LENGTH_SHORT).show();
+        Log.i("Action", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show();
+        Log.i("Action", "onDestroy");
     }
 }
